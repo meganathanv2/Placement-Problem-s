70a009b1d557a6ea6c6595e881d120dd7d468dc9
meganathanv2 <meganathan.v2022cse@sece.ac.in>
2024-08-26 18:30:40 +0530

    second commit

---
+++ A program to read a float and print it with two decimal points.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+      Scanner scan=new Scanner(System.in);
+      System.out.println("Enter the float value: ");
+      float a=scan.nextFloat();
+     System.out.printf("The value is %.2f",a);
+    }
+}
